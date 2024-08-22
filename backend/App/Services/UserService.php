3c3733ba11aaf708e9e4d0d38611fded47df5139
Sandro Souza <99900876+SandroSouza@users.noreply.github.com>
2024-08-22 14:27:46 -0300

    Add files via upload

--- backend/App/Services/UserService.php
+++ backend/App/Services/UserService.php
@@ -35,14 +35,21 @@
             if (json_last_error() !== JSON_ERROR_NONE) {
                 throw new \Exception('Dados devem ter formato json');
             } 
-            
-            $this->user->variables = $data;
 
-            $this->user->create($data);
-
-           
-            
-            return $this->user->db->lastInsertId();
+            // Verifique a ação e chame o método apropriado
+            if (isset($data['action'])) {
+                if ($data['action'] === 'login') {
+                    unset($data['action']); // Remove o campo 'action' antes de tentar salvar
+                    return $this->login($data);
+                } elseif ($data['action'] === 'register') {
+                    unset($data['action']); // Remove o campo 'action' antes de tentar salvar
+                    return $this->register($data);
+                } else {
+                    throw new \Exception('Ação desconhecida');
+                }
+            } else {
+                throw new \Exception('Ação não especificada');
+            }
         }
 
         public function put() 
@@ -89,5 +96,40 @@
         
 
             return $this->user->delete();
+        }
+
+        public function login($data)
+        {
+            $email = $data['email'];
+            $senha = $data['senha'];
+
+            // Defina os campos e o valor para a busca
+            $fields = $email;
+
+            // Chame a função search para buscar o usuário
+            $result = $this->user->findByEmail($fields);
+
+            // Verifica se algum usuário foi encontrado
+            if ($result && !empty($result)) {
+                $user = $result;
+
+                // Comparação direta da senha
+                if ($senha === $user['senha']) {
+                    return ['message' => 'Login realizado com sucesso!', 'user_id' => $user['id']];
+                } else {
+                    return ['message' => 'Email ou senha inválidos'];
+                }
+            } else {
+                return ['message' => 'Email ou senha inválidos'];
+            }
+        }
+
+
+        private function register($data)
+        {
+            $this->user->variables = $data;
+            $this->user->create($data);
+            
+            return $this->user->db->lastInsertId();
         }
     }
