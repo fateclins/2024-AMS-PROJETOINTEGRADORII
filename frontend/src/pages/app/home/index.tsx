43df11f3894142ecfc804f94a432ad9ebb658ccc
migueelzz <miguellemes005@gmail.com>
2024-09-05 14:41:01 -0300

    Landing Page - Lojista

--- frontend/src/pages/app/home/index.tsx
+++ frontend/src/pages/app/home/index.tsx
@@ -1,10 +1,8 @@
-import { Helmet } from 'react-helmet-async'
-
-import { Button } from '@/components/button'
+import { Link } from "react-router-dom";
 
 export function Home() {
   return (
-    <div className="relative overflow-hidden h-screen bg-[#22262B] p-8 flex items-center w-full mx-auto">
+    <div className="relative overflow-hidden h-screen bg-zinc-900 p-8 flex items-center w-full mx-auto">
       
       {/* Gradiente */}
       <div className="absolute -bottom-[600px] left-[50%] -translate-x-[50%] bg-[#216CC2] 
@@ -23,34 +21,31 @@ export function Home() {
               Nós criamos soluções personalizadas de e-commerce para impulsionar o crescimento da sua marca.
               </p>
               <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
-                <a
-                  href="../../login"                  
+                <Link
+                  to="/login"                  
                   className="grid grid-flow-col auto-cols-max rounded-md bg-white px-10 py-2.5 text-lg font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
                 >
                   Login
                     
-                </a>                
+                </Link>                
 
-                <a
-                  href="../../register"                  
-                  className="px-10 py-2.5 text-lg font-semibold text-white shadow-sm hover:bg-gray-900 border border-white rounded-md"
+                <Link
+                  to="/register"                  
+                  className="px-10 py-2.5 text-lg font-semibold text-white shadow-sm hover:bg-zinc-800 border border-white rounded-md"
                 >
                   Cadastrar-se
-                </a>  
+                </Link>  
 
 
               </div>
             </div>
             <img
                 alt="Firsttech"
-                src="./public/logo-no-bg.png"
+                src="/logo-no-bg.png"
                 className="w-[57rem] h-80"
               />
           </div>
       </div>
-
     </div>
-    
-
   )
 }
